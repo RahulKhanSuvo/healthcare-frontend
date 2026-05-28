cf2b1663b2053e11b08d4045dd3a0b5d123c59a8
Rahul Khan <rahul.khan.suvo@gmail.com>
2026-05-28 21:34:10 +0600

    feat: implement ConsultationPage with doctor fetching and display

--- src/app/(common layout)/consultation/page.tsx
+++ src/app/(common layout)/consultation/page.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
+import { getDoctors } from "./_actions";
+import DoctorsList from "@/components/modules/auth/consultation/DoctorsList";
 
-function ConsultationPage() {
-  return <div>ConsultationPage</div>;
+async function ConsultationPage() {
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ["doctors"],
+    queryFn: getDoctors,
+  });
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <DoctorsList />
+    </HydrationBoundary>
+  );
 }
 
 export default ConsultationPage;
