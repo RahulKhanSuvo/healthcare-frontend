a740e57d2356b52b1a147bdadca3889394aff6ee
Rahul Khan <rahul.khan.suvo@gmail.com>
2026-06-28 19:07:53 +0600

    style: fix formatting in QueryProvider and proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,8 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function proxy(request: NextRequest) {
-    console.log(request)
+  console.log(request);
 }
 export const config = {
-    matcher: '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|.well-known).*)',
-}
+  matcher:
+    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|.well-known).*)",
+};
