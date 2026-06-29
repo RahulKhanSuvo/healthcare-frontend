a6788971b62e8f670e3419de68180cfd7e7988cf
Rahul Khan <rahul.khan.suvo@gmail.com>
2026-06-29 10:20:07 +0600

    feat: add jwt utility service
    Implement JwtUtils for signing, verifying, and decoding tokens with
    support for custom role payloads.

---
+++ src/lib/jwt.ts
@@ -0,0 +1,38 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import jwt, { JwtPayload, SignOptions } from "jsonwebtoken";
+interface CustomJwtPayload extends JwtPayload {
+  role: string;
+}
+
+const createToken = (
+  payload: CustomJwtPayload,
+  secret: string,
+  { expiresIn }: SignOptions,
+) => {
+  const token = jwt.sign(payload, secret, { expiresIn });
+  return token;
+};
+const verifyToken = (token: string, secret: string) => {
+  try {
+    const decoded = jwt.verify(token, secret) as CustomJwtPayload;
+    return {
+      success: true,
+      data: decoded,
+    };
+  } catch (error: any) {
+    return {
+      success: false,
+      message: error.message,
+      error,
+    };
+  }
+};
+const decodeToken = (token: string) => {
+  const decoded = jwt.decode(token) as CustomJwtPayload;
+  return decoded;
+};
+export const JwtUtils = {
+  createToken,
+  verifyToken,
+  decodeToken,
+};
