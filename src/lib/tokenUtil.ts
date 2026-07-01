03114fa53c01262a94601559a2605ac0c3fbb251
Rahul Khan <rahul.khan.suvo@gmail.com>
2026-07-01 11:24:22 +0600

    refactor: remove unnecessary use server directives and update token logic

--- src/lib/tokenUtil.ts
+++ src/lib/tokenUtil.ts
@@ -1,24 +1,32 @@
-"use server"
-import jwt, { JwtPayload } from "jsonwebtoken"
+import jwt, { JwtPayload } from "jsonwebtoken";
 import { setCookie } from "./cookiesUtils";
-const JWT_ACCESS_SECRET = process.env.JWT_ACCESS_SECRET
+const JWT_ACCESS_SECRET = process.env.JWT_ACCESS_SECRET;
 const getTokenSecondRemaining = (token: string): number => {
-    if (!token || token === null) return 0;
-    try {
-        const tokenPayload = JWT_ACCESS_SECRET ? jwt.verify(token, JWT_ACCESS_SECRET) as JwtPayload : jwt.decode(token) as JwtPayload
-        if (!tokenPayload || !tokenPayload.exp) {
-            return 0
-        }
-        const remainingSecond = tokenPayload.exp as number - Math.floor(Date.now() / 1000)
-        return remainingSecond > 0 ? remainingSecond : 0
-
-    } catch (error) {
-        console.log(error)
-        return 0
-
+  if (!token || token === null) return 0;
+  try {
+    const tokenPayload = JWT_ACCESS_SECRET
+      ? (jwt.verify(token, JWT_ACCESS_SECRET) as JwtPayload)
+      : (jwt.decode(token) as JwtPayload);
+    if (!tokenPayload || !tokenPayload.exp) {
+      return 0;
     }
-}
-export const setTokenInCookies = async (name: string, token: string, fallback = 60 * 60 * 24) => {
-    const maxAgeSeconds = getTokenSecondRemaining(token);
-    await setCookie(name, token, maxAgeSeconds ? maxAgeSeconds : fallback)
-}
+    const remainingSecond =
+      (tokenPayload.exp as number) - Math.floor(Date.now() / 1000);
+    return remainingSecond > 0 ? remainingSecond : 0;
+  } catch (error) {
+    console.log(error);
+    return 0;
+  }
+};
+export const setTokenInCookies = async (
+  name: string,
+  token: string,
+  fallback = 60 * 60 * 24,
+) => {
+  let maxAgeSeconds = 0;
+  if (name !== "batter-auth.session_token") {
+    maxAgeSeconds = getTokenSecondRemaining(token);
+  }
+
+  await setCookie(name, token, maxAgeSeconds ? maxAgeSeconds : fallback);
+};
