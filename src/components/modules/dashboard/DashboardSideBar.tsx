471fcc60cd098ee9ea806a812a85d0aec5cd24e7
Rahul Khan <rahul.khan.suvo@gmail.com>
2026-07-03 23:30:35 +0600

    feat: implement sidebar content in dashboard sidebar

--- src/components/modules/dashboard/DashboardSideBar.tsx
+++ src/components/modules/dashboard/DashboardSideBar.tsx
@@ -2,12 +2,21 @@ import { getDefaultDashboardRoute } from "@/lib/authUtils";
 import { getNavItemsByRole } from "@/lib/navItem";
 import { getUserInfo } from "@/services/auth.service";
 import { NavSection } from "@/types/dashbaord.type";
+import DashboardSidebarContent from "./DashboardContent";
 
 const DashboardSideBar = async () => {
   const userInfo = await getUserInfo();
   const navItems: NavSection[] = getNavItemsByRole(userInfo.role);
   console.log("useInfo", userInfo);
   const dashboard = getDefaultDashboardRoute(userInfo.role);
-  return <div></div>;
+  return (
+    <div>
+      <DashboardSidebarContent
+        dashbaordHome={dashboard}
+        userInfo={userInfo}
+        navItems={navItems}
+      />
+    </div>
+  );
 };
 export default DashboardSideBar;
