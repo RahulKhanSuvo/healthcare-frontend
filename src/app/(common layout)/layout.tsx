54489201d14fc7c4da2f4908790409373cb8a931
Rahul Khan <rahul.khan.suvo@gmail.com>
2026-07-03 19:28:58 +0600

    ui: update dashboard layout structure
    Remove placeholder from common layout and implement sidebar and navbar
    structure in dashboard layout.

--- src/app/(common layout)/layout.tsx
+++ src/app/(common layout)/layout.tsx
@@ -3,10 +3,5 @@ export default function CommonLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <>
-      {" "}
-      <div> common layout</div> {children}
-    </>
-  );
+  return <>{children}</>;
 }
--- src/app/(dashboardlayout)/layout.tsx
+++ src/app/(dashboardlayout)/layout.tsx
@@ -1,7 +1,23 @@
+import DashboardNavbar from "@/components/modules/dashboard/DashboardNavbar";
+import DashboardSideBar from "@/components/modules/dashboard/DashboardSideBar";
+
 export default function DashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <>{children}</>;
+  return (
+    <div className="flex h-screen overflow-hidden">
+      {/*dashboard sidebar*/}
+      <DashboardSideBar />
+      <div className="flex flex-1 flex-col overflow-hidden">
+        {/*dashbaord navbar*/}
+        <DashboardNavbar />
+        {/*dashboard content*/}
+        <main className="flex-1 overflow-y-auto bg-muted/10 p-4 md:p-6">
+          {children}
+        </main>
+      </div>
+    </div>
+  );
 }
