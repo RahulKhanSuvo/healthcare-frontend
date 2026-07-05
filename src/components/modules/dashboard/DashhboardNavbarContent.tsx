c1912be54dd4b908f7f0061798e28b2cf1df7b8f
Rahul Khan Suvo <rahul.khan.suvo@gmail.com>
2026-07-05 17:42:45 +0600

    feat: add Sheet component to mobile navbar
    Refactor the mobile menu toggle to use the UI Sheet component and pass
    required props to the MobileSideBar.

--- src/components/modules/dashboard/DashhboardNavbarContent.tsx
+++ src/components/modules/dashboard/DashhboardNavbarContent.tsx
@@ -1,12 +1,38 @@
 import { useState } from "react";
 import MobileSideBar from "./MobileSideBar";
-
-const DashhboardNavbarContent = () => {
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Button } from "@/components/ui/button";
+import { Menu } from "lucide-react";
+import { UserInfo } from "@/types/user.types";
+import { NavSection } from "@/types/dashbaord.type";
+interface MobileSideBarProps {
+  userInfo: UserInfo;
+  navItems: NavSection[];
+  dashboardHome: string;
+}
+const DashhboardNavbarContent = ({
+  dashboardHome,
+  userInfo,
+  navItems,
+}: MobileSideBarProps) => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState<boolean>(false);
   return (
     <div>
       {/*moble menu toggle*/}
-      <MobileSideBar />
+      <Sheet open={isMobileMenuOpen} onOpenChange={setIsMobileMenuOpen}>
+        <SheetTrigger asChild className="md:hidden">
+          <Button variant={"outline"} size={"icon"}>
+            <Menu className="size-5" />
+          </Button>
+        </SheetTrigger>
+        <SheetContent>
+          <MobileSideBar
+            dashboardHome={dashboardHome}
+            userInfo={userInfo}
+            navItems={navItems}
+          />
+        </SheetContent>
+      </Sheet>
       {/*search component*/}
       {/*right side actions*/}
       {/*notifications*/}
