43a3f033bb10fc0ec13e55c6e3dc98942adb7bb0
Rahul Khan Suvo <rahul.khan.suvo@gmail.com>
2026-07-12 01:24:46 +0600

    refactor: remove deprecated getDoctor service

--- src/services/auth.service.ts
+++
@@ -1,8 +0,0 @@
-"use server";
-
-import { httpClient } from "@/lib/axios/httpClient";
-
-export const getDoctor = async () => {
-  const response = await httpClient.get("/doctors");
-  return response.data;
-};
