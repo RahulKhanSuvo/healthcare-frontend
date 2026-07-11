a45f7b36b4f72143d4a6905b0c88661bda4b5a20
Rahul Khan Suvo <rahul.khan.suvo@gmail.com>
2026-07-11 17:15:45 +0600

    feat: add notifications dropdown to dashboard navbar

--- src/components/modules/dashboard/DashhboardNavbarContent.tsx
+++ src/components/modules/dashboard/DashhboardNavbarContent.tsx
@@ -7,6 +7,7 @@ import { Menu, Search } from "lucide-react";
 import { UserInfo } from "@/types/user.types";
 import { NavSection } from "@/types/dashbaord.type";
 import { Input } from "@/components/ui/input";
+import NotificationsDropdown from "./NotificationsDropdown";
 interface MobileSideBarProps {
   userInfo: UserInfo;
   navItems: NavSection[];
@@ -19,7 +20,7 @@ const DashhboardNavbarContent = ({
 }: MobileSideBarProps) => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState<boolean>(false);
   return (
-    <div className="bg-gray-100">
+    <div className="bg-gray-100 flex items-center border-red-400 border justify-between">
       {/*moble menu toggle*/}
       <Sheet open={isMobileMenuOpen} onOpenChange={setIsMobileMenuOpen}>
         <SheetTrigger asChild className="md:hidden">
@@ -36,7 +37,7 @@ const DashhboardNavbarContent = ({
         </SheetContent>
       </Sheet>
       {/*search component*/}
-      <div className="flex-1 flex items-center justify-end gap-1">
+      <div className="w-fit  flex items-center justify-end gap-1">
         <div className="relative w-full max-w-md hidden sm:block">
           <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 size-5" />
           <Input type="text" placeholder="Search" className="pl-9 pr-4" />
@@ -45,6 +46,7 @@ const DashhboardNavbarContent = ({
       {/*right side actions*/}
 
       {/*notifications*/}
+      <NotificationsDropdown/>
       {/*user dropdown*/}
     </div>
   );
