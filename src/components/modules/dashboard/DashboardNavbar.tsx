1000626380750c9d4494dc2cc1205de551372ae7
Rahul Khan Suvo <rahul.khan.suvo@gmail.com>
2026-07-11 16:37:43 +0600

    refactor: replace MobileSideBar with DashhboardNavbarContent

--- src/components/modules/dashboard/DashboardNavbar.tsx
+++ src/components/modules/dashboard/DashboardNavbar.tsx
@@ -3,6 +3,7 @@ import { NavSection } from "@/types/dashbaord.type";
 import { getNavItemsByRole } from "@/lib/navItem";
 import { getDefaultDashboardRoute } from "@/lib/authUtils";
 import MobileSideBar from "./MobileSideBar";
+import DashhboardNavbarContent from "./DashhboardNavbarContent";
 
 const DashboardNavbar = async () => {
   const userInfo = await getUserInfo();
@@ -10,11 +11,13 @@ const DashboardNavbar = async () => {
   console.log("useInfo", userInfo);
   const dashboard = getDefaultDashboardRoute(userInfo.role);
   return (
-    <MobileSideBar
-      navItems={navItems}
-      userInfo={userInfo}
-      dashboardHome={dashboard}
-    />
+    // <MobileSideBar
+    //   navItems={navItems}
+    //   userInfo={userInfo}
+    //   dashboardHome={dashboard}
+    // />
+    <DashhboardNavbarContent navItems={navItems} dashboardHome={dashboard}
+    userInfo={userInfo}/>
   );
 };
 export default DashboardNavbar;
