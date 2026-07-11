0ca3030add40f452c9f4f847c8a8831cf967a8d1
Rahul Khan Suvo <rahul.khan.suvo@gmail.com>
2026-07-11 16:06:53 +0600

    refactor: flatten user data in ILoginResponse interface

--- src/types/auth.type.ts
+++ src/types/auth.type.ts
@@ -2,21 +2,19 @@ export interface ILoginResponse {
   token: string;
   accessToken: string;
   refreshToken: string;
-  data: {
-    user: {
-      id: string;
-      email: string;
-      name: string;
-      role: string;
-      image: string | null;
-      status: string;
-      emailVerified: boolean;
-      needPasswordChange: boolean;
-      createdAt: string;
-      updatedAt: string;
-      isDeleted: boolean;
-      deletedAt: string | null;
-    };
-    redirect: boolean;
+  redirect: boolean;
+  user: {
+    id: string;
+    email: string;
+    name: string;
+    role: string;
+    image: string | null;
+    status: string;
+    emailVerified: boolean;
+    needPasswordChange: boolean;
+    createdAt: string;
+    updatedAt: string;
+    isDeleted: boolean;
+    deletedAt: string | null;
   };
 }
