3d910fca32131288f399acf1c75bdd234e5ea22f
Rahul Khan Suvo <rahul.khan.suvo@gmail.com>
2026-07-11 23:50:53 +0600

    feat: implement server-side data fetching for admin dashboard
    Use React Query's prefetching and hydration to load initial dashboard
    data on the server.

--- src/app/(dashboardlayout)/admin/dashboard/page.tsx
+++ src/app/(dashboardlayout)/admin/dashboard/page.tsx
@@ -1,4 +1,18 @@
-const AdminDashboardPage = () => {
-  return <div>AdminDashboardPage</div>;
+import AdminDashboardContent from "@/components/modules/dashboard/AdminDashboardContent";
+import { getDashboardData } from "@/services/dashboard.service";
+import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query";
+
+const AdminDashboardPage = async () => {
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ["admin-dashboard-data"],
+    queryFn: getDashboardData,
+  })
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <AdminDashboardContent />
+    </HydrationBoundary>
+  );
 };
 export default AdminDashboardPage;
