13ac8a909d0b5f4a5824eadbc5b54fa3336845d1
Rahul Khan Suvo <rahul.khan.suvo@gmail.com>
2026-07-12 20:53:07 +0600

    feat: update UserInfo interface with additional user fields

--- src/types/user.types.ts
+++ src/types/user.types.ts
@@ -2,7 +2,15 @@ import { UserRole } from "@/lib/authUtils";
 
 export interface UserInfo {
   id: string;
-  name: string;
   email: string;
+  name: string;
   role: UserRole;
+  image: string | null;
+  status: string;
+  emailVerified: boolean;
+  needPasswordChange: boolean;
+  createdAt: string;
+  updatedAt: string;
+  isDeleted: boolean;
+  deletedAt: string | null;
 }
