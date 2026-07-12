d9c782640c146f46e6731dcbe41bd0d1d841a7c8
Rahul Khan Suvo <rahul.khan.suvo@gmail.com>
2026-07-12 17:30:11 +0600

    refactor: add authentication access rules to proxy middleware

--- src/proxy.ts
+++ src/proxy.ts
@@ -19,8 +19,6 @@ export function proxy(request: NextRequest) {
           process.env.ACCESS_TOKEN_SECRET as string,
         ).data
       : null;
-    console.log("data", decodedAccessToken);
-
     const isValidAccessToken = accessToken
       ? jwtUtils.verifyToken(
           accessToken,
@@ -37,18 +35,29 @@ export function proxy(request: NextRequest) {
     const routeOwner = getRouteOwner(pathname);
     const unifiedRole = userRole === "SUPER_ADMIN" ? "ADMIN" : userRole;
     const isAuth = isAuthRoute(pathname);
-    console.log("isAuthRoute", isAuth);
     console.log("Path:", pathname);
     console.log("Route Owner:", routeOwner);
     console.log("Role:", unifiedRole);
+    console.log("isAuthRoute", isAuth);
     console.log("isValid use", isValidAccessToken);
 
+    // rule:1 if user is login but try to access to auth route
     if (isAuth && isValidAccessToken) {
       return NextResponse.redirect(
         new URL(getDefaultDashboardRoute(userRole), request.url),
       );
     }
+    // rule:2- if user try to access to the public route
+    if (routeOwner === null) {
+      return NextResponse.next();
+    }
 
+    // rule3: if use is not login
+    if (!accessToken) {
+      const loginUrl = new URL("/login", request.url);
+      loginUrl.searchParams.set("redirect", pathname);
+      return NextResponse.redirect(loginUrl);
+    }
     return NextResponse.next();
   } catch (error) {
     console.error("Proxy Error:", error);
