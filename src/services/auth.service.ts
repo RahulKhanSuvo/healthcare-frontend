63e0e8cb35b77a70455119af0ecfb35e6d9feeb1
Rahul Khan Suvo <rahul.khan.suvo@gmail.com>
2026-07-12 19:16:11 +0600

    feat: implement refresh token logic in auth service

---
+++ src/services/auth.service.ts
@@ -0,0 +1,43 @@
+"use server";
+
+import { setTokenInCookies } from "@/lib/tokenUtil";
+
+const BASE_API_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+if (!BASE_API_URL) {
+  throw new Error("NEXT_PUBLIC_API_BASE_URL is not defined");
+}
+
+export async function newRefreshToken(refreshToken: string): Promise<boolean> {
+  try {
+    const response = await fetch(`${BASE_API_URL}/auth/refresh-token`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Cookie: `refreshToken=${refreshToken}`,
+      },
+    });
+    if (!response.ok) {
+      return false;
+    }
+    const { data } = await response.json();
+    const {
+      refreshToken: newRefreshToken,
+      accessToken: newAccessToken,
+      token: sessionToken,
+    } = data;
+    if (newAccessToken) {
+      setTokenInCookies("batter-auth.session_token", sessionToken);
+    }
+    if (newRefreshToken) {
+      setTokenInCookies("refreshToken", newRefreshToken);
+    }
+    if (newAccessToken) {
+      setTokenInCookies("accessToken", newAccessToken);
+    }
+
+    return true;
+  } catch (error) {
+    console.error("Error: error message", error);
+    return false;
+  }
+}
