0ced088bd0994af960c13e35262d1f430d276c2f
Rahul Khan Suvo <rahul.khan.suvo@gmail.com>
2026-07-12 20:53:04 +0600

    feat: add getUserInfo service method

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,6 +1,8 @@
 "use server";
 
 import { setTokenInCookies } from "@/lib/tokenUtil";
+import { UserInfo } from "@/types/user.types";
+import { cookies } from "next/headers";
 
 const BASE_API_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 if (!BASE_API_URL) {
@@ -42,4 +44,25 @@ export async function getNewRefreshToken(
     console.error("Error: error message", error);
     return false;
   }
+}
+// get current user
+export async function getUserInfo(): Promise<UserInfo | null> {
+  const cookieStore = await cookies();
+  const sessionToken = cookieStore.get("batter-auth.session_token")?.value;
+  const accessToken = cookieStore.get("accessToken")?.value;
+  if (!sessionToken || !accessToken) {
+    return null;
+  }
+  const response = await fetch(`${BASE_API_URL}/auth/me`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Cookie: `batter-auth.session_token=${sessionToken}; accessToken=${accessToken}`,
+    },
+  });
+  if (!response.ok) {
+    return null;
+  }
+  const { data } = await response.json();
+  return data as UserInfo;
 }
