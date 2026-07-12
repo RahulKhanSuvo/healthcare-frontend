2dd21ce66785b5367999668ee5a992f71f5415bc
Rahul Khan Suvo <rahul.khan.suvo@gmail.com>
2026-07-12 12:48:18 +0600

    refactor: comment out dashboard sidebar and navbar components

--- src/app/(dashboardlayout)/layout.tsx
+++ src/app/(dashboardlayout)/layout.tsx
@@ -9,10 +9,10 @@ export default function DashboardLayout({
   return (
     <div className="flex h-screen overflow-hidden">
       {/*dashboard sidebar*/}
-      <DashboardSideBar />
+      {/*<DashboardSideBar />*/}
       <div className="flex flex-1 flex-col overflow-hidden">
         {/*dashbaord navbar*/}
-        <DashboardNavbar />
+        {/*<DashboardNavbar />*/}
         {/*dashboard content*/}
         <main className="flex-1 overflow-y-auto bg-muted/10 p-4 md:p-6">
           {children}
