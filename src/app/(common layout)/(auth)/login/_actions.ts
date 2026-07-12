c55e92e8ab96d05e600ca0c0a268e14e97c4c974
Rahul Khan Suvo <rahul.khan.suvo@gmail.com>
2026-07-12 09:56:19 +0600

    feat: implement login server action
    Add loginAction to handle authentication requests with validation and
    error handling.

---
+++ src/app/(common layout)/(auth)/login/_actions.ts
@@ -0,0 +1,31 @@
+import { httpClient } from "@/lib/axios/httpClient";
+import { ApiErrorResponse } from "@/types/api.type";
+import { ILoginResponse } from "@/types/auth.type";
+import { ILoginPayload, loginZodSchema } from "@/zod/auth.validation";
+
+export const loginAction = async (
+  payload: ILoginPayload,
+): Promise<ILoginResponse | ApiErrorResponse> => {
+  const parsedPayload = loginZodSchema.safeParse(payload);
+  if (!parsedPayload.success) {
+    const errorMessage =
+      parsedPayload.error.issues[0].message || "Invalid payload";
+    return {
+      success: false,
+      message: errorMessage,
+    };
+  }
+  try {
+    const response = await httpClient.post<ILoginResponse>(
+      "auth/login",
+      payload,
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Error: error message", error);
+    return {
+      success: false,
+      message: `Login failed: ${error instanceof Error ? error.message : String(error)}`,
+    };
+  }
+};
