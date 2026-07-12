a21a9b1bec6afac864bea0336fea9096f669e256
Rahul Khan Suvo <rahul.khan.suvo@gmail.com>
2026-07-12 17:06:52 +0600

    feat: implement auth logic in proxy middleware
    Add token verification and role-based route redirection for
    authenticated
    users, and fix the matcher regex to correctly handle the .well-known
    path.

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,18 +1,61 @@
 import { NextRequest, NextResponse } from "next/server";
+import { jwtUtils } from "./lib/jwt";
+import {
+  getDefaultDashboardRoute,
+  getRouteOwner,
+  isAuthRoute,
+  UserRole,
+} from "./lib/authUtils";
 
 export function proxy(request: NextRequest) {
-  // return NextResponse.redirect(new URL("/home", request.url));
-  console.log(request);
+  try {
+    const { pathname } = request.nextUrl;
+
+    const accessToken = request.cookies.get("accessToken")?.value;
+
+    const decodedAccessToken = accessToken
+      ? jwtUtils.verifyToken(
+          accessToken,
+          process.env.JWT_ACCESS_SECRET as string,
+        ).data
+      : null;
+
+    const isValidAccessToken = accessToken
+      ? jwtUtils.verifyToken(
+          accessToken,
+          process.env.JWT_ACCESS_SECRET as string,
+        ).success
+      : false;
+
+    let userRole: UserRole | null = null;
+
+    if (decodedAccessToken) {
+      userRole = decodedAccessToken.role as UserRole;
+    }
+
+    const routeOwner = getRouteOwner(pathname);
+    const unifiedRole = userRole === "SUPER_ADMIN" ? "ADMIN" : userRole;
+    const isAuth = isAuthRoute(pathname);
+    console.log("isAuthRoute", isAuth);
+    console.log("Path:", pathname);
+    console.log("Route Owner:", routeOwner);
+    console.log("Role:", unifiedRole);
+
+    if (isAuth && isValidAccessToken) {
+      return NextResponse.redirect(
+        new URL(getDefaultDashboardRoute(userRole), request.url),
+      );
+    }
+
+    return NextResponse.next();
+  } catch (error) {
+    console.error("Proxy Error:", error);
+    return NextResponse.next();
+  }
 }
+
 export const config = {
   matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
-     */
-    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|.well-known).*)",
+    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|\\.well-known).*)",
   ],
 };
