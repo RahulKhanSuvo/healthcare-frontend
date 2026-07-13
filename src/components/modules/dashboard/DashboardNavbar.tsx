42b1cdb3b6dc0e51a99756b085e1d62498dcb869
Rahul Khan Suvo <rahul.khan.suvo@gmail.com>
2026-07-13 12:08:44 +0600

    feat: enable sidebar and navbar components in dashboard

--- src/components/modules/dashboard/DashboardNavbar.tsx
+++ src/components/modules/dashboard/DashboardNavbar.tsx
@@ -1,24 +1,29 @@
-// import { getUserInfo } from "@/services/auth.service";
-// import { NavSection } from "@/types/dashbaord.type";
-// import { getNavItemsByRole } from "@/lib/navItem";
-// import { getDefaultDashboardRoute } from "@/lib/authUtils";
-// import DashhboardNavbarContent from "./DashhboardNavbarContent";
+import { NavSection } from "@/types/dashbaord.type";
+import { getNavItemsByRole } from "@/lib/navItem";
+import { getDefaultDashboardRoute } from "@/lib/authUtils";
+import DashhboardNavbarContent from "./DashhboardNavbarContent";
+import MobileSideBar from "./MobileSideBar";
+import { getUserInfo } from "@/services/auth.service";
 
 const DashboardNavbar = async () => {
-  // const userInfo = await getUserInfo();
-  // const navItems: NavSection[] = getNavItemsByRole(userInfo.role);
-  // console.log("useInfo", userInfo);
-  // const dashboard = getDefaultDashboardRoute(userInfo.role);
+  const userInfo = await getUserInfo();
+  const navItems: NavSection[] = getNavItemsByRole(userInfo.role);
+  console.log("useInfo", userInfo);
+  const dashboard = getDefaultDashboardRoute(userInfo.role);
   return (
-    // <MobileSideBar
-    //   navItems={navItems}
-    //   userInfo={userInfo}
-    //   dashboardHome={dashboard}
-    // />
-    // <DashhboardNavbarContent navItems={navItems} dashboardHome={dashboard}
-    // userInfo={userInfo}/>
     <div>
-      <nav>nanfg</nav>
+      <nav>
+        <MobileSideBar
+          navItems={navItems}
+          userInfo={userInfo}
+          dashboardHome={dashboard}
+        />
+        <DashhboardNavbarContent
+          navItems={navItems}
+          dashboardHome={dashboard}
+          userInfo={userInfo}
+        />
+      </nav>
     </div>
   );
 };
