cb19293d492ea3fd06fabbef9a9f4cc452c59aaa
Rahul Khan Suvo <rahul.khan.suvo@gmail.com>
2026-07-13 11:51:44 +0600

    refactor: replace admin dashboard implementation with placeholder

--- src/app/(dashboardlayout)/admin/dashboard/page.tsx
+++ src/app/(dashboardlayout)/admin/dashboard/page.tsx
@@ -1,19 +1,25 @@
-import AdminDashboardContent from "@/components/modules/dashboard/AdminDashboardContent";
-import { getDashboardData } from "@/services/dashboard.service";
-import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query";
+// import AdminDashboardContent from "@/components/modules/dashboard/AdminDashboardContent";
+// import { getDashboardData } from "@/services/dashboard.service";
+// import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query";
 
-const AdminDashboardPage = async () => {
-  const queryClient = new QueryClient();
-  await queryClient.prefetchQuery({
-    queryKey: ["admin-dashboard-data"],
-    queryFn: getDashboardData,
-  })
-  const dashboardData = await queryClient.getQueryData(["admin-dashboard-data"]);
-  console.log("dashbord",dashboardData)
-  return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <AdminDashboardContent />
-    </HydrationBoundary>
-  );
+// const AdminDashboardPage = async () => {
+//   const queryClient = new QueryClient();
+//   await queryClient.prefetchQuery({
+//     queryKey: ["admin-dashboard-data"],
+//     queryFn: getDashboardData,
+//   })
+//   const dashboardData = await queryClient.getQueryData(["admin-dashboard-data"]);
+//   console.log("dashbord",dashboardData)
+//   return (
+//     <HydrationBoundary state={dehydrate(queryClient)}>
+//       <AdminDashboardContent />
+//     </HydrationBoundary>
+//   );
+// };
+// export default AdminDashboardPage;
+
+const AdminDashboard = () => {
+  return <div>AdminDashboard</div>;
 };
-export default AdminDashboardPage;
+
+export default AdminDashboard;
