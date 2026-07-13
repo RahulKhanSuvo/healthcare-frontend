e81b19fb41ab325916a1a384a8e5e9c56753ca99
Rahul Khan Suvo <rahul.khan.suvo@gmail.com>
2026-07-13 23:46:08 +0600

    feat: fetch admin dashboard data using react-query

--- src/components/modules/dashboard/AdminDashboardContent.tsx
+++ src/components/modules/dashboard/AdminDashboardContent.tsx
@@ -1,4 +1,17 @@
+"use client";
+import { getDashboardData } from "@/services/dashboard.service";
+import { useQuery } from "@tanstack/react-query";
+
 const AdminDashboardContent = () => {
+  const { data: dashboardData } = useQuery({
+    queryKey: ["admin-dashboard-data"],
+    queryFn: getDashboardData,
+    refetchOnWindowFocus: true,
+  });
+
+  // Now dashboardData is ApiResponse<IAdminDashboardData>
+  const data = dashboardData?.data;
+  console.log(data);
   return <div>AdminDashboardContent</div>;
 };
 
