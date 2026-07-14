b9a9dc843493971836e74bc951a8ed87a4b0a0c5
Rahul Khan Suvo <rahul.khan.suvo@gmail.com>
2026-07-14 12:21:54 +0600

    feat: prefetch doctors data in management page

--- src/app/(dashboardlayout)/admin/dashboard/doctors-management/page.tsx
+++ src/app/(dashboardlayout)/admin/dashboard/doctors-management/page.tsx
@@ -1,4 +1,14 @@
-const DoctorsManagementPage = () => {
+import { getDoctors } from "@/services/doctor.service";
+import { QueryClient } from "@tanstack/react-query";
+
+const DoctorsManagementPage = async () => {
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ["doctors"],
+    queryFn: getDoctors,
+    staleTime: 1000 * 60 * 5,
+    gcTime: 1000 * 60 * 5,
+  });
   return <div>DoctorsManagementPage</div>;
 };
 export default DoctorsManagementPage;
