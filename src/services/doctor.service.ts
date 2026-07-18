92056070895b58f996036a8d906beee7707b96ad
Rahul Khan Suvo <rahul.khan.suvo@gmail.com>
2026-07-18 18:26:25 +0600

    fix: add error handling to getDoctors service

--- src/services/doctor.service.ts
+++ src/services/doctor.service.ts
@@ -3,6 +3,10 @@ import { httpClient } from "@/lib/axios/httpClient";
 import { IDoctor } from "@/types/doctors.type";
 
 export const getDoctors = async () => {
-  const res = await httpClient.get<IDoctor[]>("/doctors");
-  return res.data;
+  try {
+    const res = await httpClient.get<IDoctor[]>("/doctors");
+    return res.data;
+  } catch (error) {
+    console.error("Error: error message", error)
+  }
 };
