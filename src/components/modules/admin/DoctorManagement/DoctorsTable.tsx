ae86daa4d5d4e80db2b18d1951b4793ad04952d1
Rahul Khan Suvo <rahul.khan.suvo@gmail.com>
2026-07-18 18:23:10 +0600

    feat: fetch doctor data in DoctorsTable

--- src/components/modules/admin/DoctorManagement/DoctorsTable.tsx
+++ src/components/modules/admin/DoctorManagement/DoctorsTable.tsx
@@ -1,6 +1,19 @@
 "use client";
+
+import { getDoctors } from "@/services/doctor.service";
+import { useQuery } from "@tanstack/react-query";
+
 const DoctorsTable = () => {
-  return <div>DoctorsTable</div>;
+  const { data:doctorDataResponse } = useQuery({
+    queryKey: ['doctors'],
+    queryFn: getDoctors,
+  })
+  console.log(doctorDataResponse)
+  return (
+    <div>
+
+    </div>
+  );
 };
 
 export default DoctorsTable;
