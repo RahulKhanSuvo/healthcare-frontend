0385f72f951c08e67b0933637c1c1c197c730abb
Rahul Khan Suvo <rahul.khan.suvo@gmail.com>
2026-07-18 22:38:14 +0600

    feat: implement TanStack Table in DoctorsTable component

--- src/components/modules/admin/DoctorManagement/DoctorsTable.tsx
+++ src/components/modules/admin/DoctorManagement/DoctorsTable.tsx
@@ -1,18 +1,64 @@
 "use client";
-
+import {
+  createColumnHelper,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
 import { getDoctors } from "@/services/doctor.service";
 import { useQuery } from "@tanstack/react-query";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 const DoctorsTable = () => {
-  const { data:doctorDataResponse } = useQuery({
-    queryKey: ['doctors'],
+  const doctorColums = [
+    { accessorKey: "name", header: "Name" },
+    { accessorKey: "specialization", header: "Specialization" },
+    { accessorKey: "experience", header: "Experience" },
+    { accessorKey: "rating", header: "Rating" },
+  ];
+  const { data: doctorDataResponse } = useQuery({
+    queryKey: ["doctors"],
     queryFn: getDoctors,
-  })
-  console.log(doctorDataResponse)
-  return (
-    <div>
+  });
+  console.log(doctorDataResponse);
+  const { getHeaderGroups, getRowModel } = useReactTable({
+    data: doctorDataResponse ?? [],
+    columns: doctorColums,
+    getCoreRowModel: getCoreRowModel(),
+  });
 
-    </div>
+  return (
+    <Table>
+      <TableHeader>
+        {getHeaderGroups().map((hg) => (
+          <TableRow key={hg.id}>
+            {hg.headers.map((header) => (
+              <TableHead key={header.id}>
+                {flexRender(header.column.columnDef.header, header.getContext())}
+              </TableHead>
+            ))}
+          </TableRow>
+        ))}
+      </TableHeader>
+      <TableBody>
+        {getRowModel().rows.map((row) => (
+          <TableRow key={row.id}>
+            {row.getVisibleCells().map((cell) => (
+              <TableCell key={cell.id}>
+                {flexRender(cell.column.columnDef.cell, cell.getContext())}
+              </TableCell>
+            ))}
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
   );
 };
 
