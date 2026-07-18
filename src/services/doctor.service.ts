fe1ea2052b555f8457b76729ab73b7249de3ca4f
Rahul Khan Suvo <rahul.khan.suvo@gmail.com>
2026-07-18 18:23:14 +0600

    feat: add IDoctor type to getDoctors service

--- src/services/doctor.service.ts
+++ src/services/doctor.service.ts
@@ -1,7 +1,8 @@
 "use server";
 import { httpClient } from "@/lib/axios/httpClient";
+import { IDoctor } from "@/types/doctors.type";
 
 export const getDoctors = async () => {
-  const res = await httpClient.get("/doctors");
+  const res = await httpClient.get<IDoctor[]>("/doctors");
   return res.data;
 };
